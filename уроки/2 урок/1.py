4405641d4859b8f15184f0beedc9d879466430bc
Максим_Intern <max20088888@mail.ru>
2023-06-25 19:49:42 +1000

    2 урок

--- уроки/2 урок/1.py
+++ уроки/2 урок/1.py
@@ -1,6 +1,2 @@
-minutes = 60 
-day = 365
-hours = 24
-sek = 60
-
-print(minutes * day * hours *   sek )
+age = 15
+city = "Vladivostok"
---
+++ уроки/2 урок/2.py
@@ -0,0 +1,6 @@
+day = 7
+hours = 24
+minutes = 60 
+sek = 60
+
+print(minutes * day * hours *  sek )
